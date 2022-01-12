a71fedff68d2bbd56fe868634bc659c6acd118f6
TikvaYosef <tikvayosef13@gmail.com>
2022-01-12 11:26:29 +0200

    Add project files.

---
+++ Controllers/API/StudentController.cs
@@ -0,0 +1,58 @@
+﻿using school.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace school.Controllers.API
+{
+    public class StudentController : Controller
+    {
+
+        Student[] arrayOfStudent = new Student[] {
+       new Student(0,"tikva","yosef",12,26),
+       new Student(1,"keren","hailu",11,23),
+       new Student(2,"eden","tasama",9,24),
+       new Student(3,"yafit","samuel",8,22)
+        };
+
+        // GET: Student
+        public ActionResult ShowStudentById(int id)
+        {
+            foreach (Student item in arrayOfStudent)
+            {
+                if (id == item.Id)
+                {
+                    ViewBag.student1 = item;
+
+                }
+
+            }
+
+            return View(ViewBag.student1);
+
+        }
+
+        // GET: Students
+        public ActionResult ShowStudents()
+        {
+            return View(arrayOfStudent);
+        }
+
+        public ActionResult ShowStudentName()
+        {
+            foreach (Student item in arrayOfStudent)
+            {
+              
+                
+                    ViewBag.name = item;
+
+                
+
+            }
+            return View(ViewBag.name);
+        }
+
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace school.Models
+{
+    public class Student
+    {
+        public int Id;
+        public string Fname;
+        public string Lname;
+        public int SchoolClass;
+        public int Age;
+
+        public Student(int id, string fname, string lname, int schoolClass, int age)
+        {
+            this.Id = id;
+            this.Fname = fname;
+            this.Lname = lname;
+            this.SchoolClass = schoolClass;
+            this.Age = age;
+        }
+    }
+}
